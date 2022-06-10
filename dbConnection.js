5664178e35023c780608d56a27bfdb2017a150fc
Debashis Kar Suvra <kar.suvra@gmail.com>
2022-06-10 10:55:43 +0600

    updated

--- dbConnection.js
+++ dbConnection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 const connect = async() => {
     try {
-        let res=await mongoose.connect("mongodb+srv://suvra123:01711536682Suv@nutriotionbee.qqsv8.mongodb.net/nutriotionBee?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, });
+        let res=await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, });
         console.log("DB Connected");
     } catch (error) {
         console.log("Not Connected ,", error);
