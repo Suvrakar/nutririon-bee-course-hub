009af5e01c58669523031f21b669b2ca0c1c7d59
Suvrakar <kar.suvra@gmail.com>
2023-03-29 10:08:26 +0600

    done

--- server.js
+++ server.js
@@ -135,18 +135,14 @@ app.get('/image/:id', async (req, res) => {
 app.delete('/image/:id', async (req, res) => {
   const imageId = req.params.id;
   nameUser = await Users.find({ email: req.user.email })
-  const mail = nameUser[0]?.email;
-  var currentdate = new Date().toLocaleDateString();
-  var currenttime = new Date().toLocaleTimeString();
-  const image = await Image.find({ email: req.user.email })
-  const id = image[0]?.id;
   Image.findByIdAndDelete(imageId)
     .then(image => {
       if (!image) {
         return res.status(404).send('Image not found');
       }
-      // res.send('Image deleted successfully', {imageId});
-      res.render('index.ejs', {id, proPicLink, currenttime, currentdate, mail, paymentStatus: req.user.paymentStatus, name: req.user.name, unvname: req.user.unvname, phone: req.user.phone })
+      // res.send("Done")
+      res.redirect("/profile")
+      // res.render('index.ejs', {id, proPicLink, currenttime, currentdate, mail, paymentStatus: req.user.paymentStatus, name: req.user.name, unvname: req.user.unvname, phone: req.user.phone })
     })
     .catch(err => {
       res.status(500).send(err);
