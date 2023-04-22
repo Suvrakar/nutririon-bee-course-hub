2c4bae376c6421d28b963372b01b3a1955f90cd0
Debashis Kar Suvra <kar.suvra@gmail.com>
2023-04-22 20:25:28 +0600

    sharp remoded

--- routes/proPic.js
+++ routes/proPic.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const commonFunc = require("../commonFunctions")
 const { Users } = require("../models/Users")
 const { ProfileImage } = require("../models/ProfileImage")
-const sharp = require('sharp');
+// const sharp = require('sharp');
 const mongoose = require("mongoose")
 const multer = require('multer');
 
@@ -24,12 +24,12 @@ router.post('/upload', upload.single('image'), async (req, res) => {
         if (!image || (image.mimetype !== 'image/jpeg' && image.mimetype !== 'image/png')) {
             throw new Error('Invalid file type. Only JPG/JPEG and PNG files are allowed.');
         }
-        const resizedImage = await sharp(image.buffer)
-            .resize({ width: 500, height: 500 })
-            .toBuffer();
+        // const resizedImage = await sharp(image.buffer)
+        //     .resize({ width: 500, height: 500 })
+        //     .toBuffer();
         const newImage = new ProfileImage({
             name: image.originalname,
-            data: resizedImage,
+            data: image.buffer,
             email: mail,
             contentType: image.mimetype
         });
